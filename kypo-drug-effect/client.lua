ac3ca9d3258d52e1111e783236d2ea7ccaf3e354
Raigato <gabriel.raymondou@gmail.com>
2021-08-29 00:25:10 +0200

    added beer

--- kypo-drug-effect/client.lua
+++ kypo-drug-effect/client.lua
@@ -31,6 +31,34 @@ function DisplayNotification( text )
 end
 
 -- // EFFECTS
+function beer()
+  local playerPed = GetPlayerPed(-1)
+  local playerPed = PlayerPedId()
+
+  RequestAnimSet("MOVE_M@DRUNK@VERYDRUNK")
+  while not HasAnimSetLoaded("MOVE_M@DRUNK@VERYDRUNK") do
+    Citizen.Wait(0)
+  end
+  DisplayNotification("You are very drunk!")
+  TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_PARTYING", 0, 1)
+  Citizen.Wait(3000)
+  ClearPedTasksImmediately(playerPed)
+  SetTimecycleModifier("spectator5")
+  SetPedMotionBlur(playerPed, true)
+  SetPedMovementClipset(playerPed, "MOVE_M@DRUNK@VERYDRUNK", true)
+  SetPedIsDrunk(playerPed, true)
+  AnimpostfxPlay("ChopVision", 10000001, true)
+  ShakeGameplayCam("DRUNK_SHAKE", 3.0)
+  Citizen.Wait(DURATION)
+-- after wait stop all 
+  SetPedIsDrunk(GetPlayerPed(-1), false)		
+  SetPedMotionBlur(playerPed, false)
+  ResetPedMovementClipset(GetPlayerPed(-1))
+  AnimpostfxStopAll()
+  ShakeGameplayCam("DRUNK_SHAKE", 0.0)
+  SetTimecycleModifierStrength(0.0)
+end
+
 function coke()
   local playerPed = GetPlayerPed(-1)
   local playerPed = PlayerPedId()
@@ -63,155 +91,155 @@ function weed()
   local playerPed = GetPlayerPed(-1)
   local playerPed = PlayerPedId()
   
-    RequestAnimSet("MOVE_M@DRUNK@VERYDRUNK") 
-    while not HasAnimSetLoaded("MOVE_M@DRUNK@VERYDRUNK") do
-      Citizen.Wait(0)
-    end    
-    DisplayNotification('You took some weed!')
-    TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_SMOKING_POT", 0, 1)
-    Citizen.Wait(3000)
-    ClearPedTasksImmediately(playerPed)
-    SetTimecycleModifier("spectator6")
-    SetPedMotionBlur(playerPed, true)
-    SetPedMovementClipset(playerPed, "MOVE_M@DRUNK@VERYDRUNK", true)
-    SetPedIsDrunk(playerPed, true)
-    AnimpostfxPlay("ChopVision", 10000001, true)
-    ShakeGameplayCam("DRUNK_SHAKE", 1.0)
+  RequestAnimSet("MOVE_M@DRUNK@VERYDRUNK") 
+  while not HasAnimSetLoaded("MOVE_M@DRUNK@VERYDRUNK") do
+    Citizen.Wait(0)
+  end    
+  DisplayNotification('You took some weed!')
+  TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_SMOKING_POT", 0, 1)
+  Citizen.Wait(3000)
+  ClearPedTasksImmediately(playerPed)
+  SetTimecycleModifier("spectator6")
+  SetPedMotionBlur(playerPed, true)
+  SetPedMovementClipset(playerPed, "MOVE_M@DRUNK@VERYDRUNK", true)
+  SetPedIsDrunk(playerPed, true)
+  AnimpostfxPlay("ChopVision", 10000001, true)
+  ShakeGameplayCam("DRUNK_SHAKE", 1.0)
 --vvvvvvvvvvvvvvvv
-    Citizen.Wait(DURATION)
+  Citizen.Wait(DURATION)
 --^^^^^^^^^^^^^^^^
 --Time of effect
 --  after wait stop all effects
-    SetPedIsDrunk(GetPlayerPed(-1), false)		
-    SetPedMotionBlur(playerPed, false)
-    ResetPedMovementClipset(GetPlayerPed(-1))
-    AnimpostfxStopAll()
-    ShakeGameplayCam("DRUNK_SHAKE", 0.0)
-    SetTimecycleModifierStrength(0.0)
+  SetPedIsDrunk(GetPlayerPed(-1), false)		
+  SetPedMotionBlur(playerPed, false)
+  ResetPedMovementClipset(GetPlayerPed(-1))
+  AnimpostfxStopAll()
+  ShakeGameplayCam("DRUNK_SHAKE", 0.0)
+  SetTimecycleModifierStrength(0.0)
 end
 
 function heroin()  
   local playerPed = GetPlayerPed(-1)
   local playerPed = PlayerPedId()
   
-    RequestAnimSet("move_m@hobo@a") 
-    while not HasAnimSetLoaded("move_m@hobo@a") do
-      Citizen.Wait(0)
-    end    
-    DisplayNotification('You took some Heroin!')
-    TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_SMOKING_POT", 0, 1)
-    Citizen.Wait(3000)
-    ClearPedTasksImmediately(playerPed)
-    SetTimecycleModifier("spectator3")
-    SetPedMotionBlur(playerPed, true)
-    SetPedMovementClipset(playerPed, "move_m@hobo@a", true)
-    SetPedIsDrunk(playerPed, true)
-    AnimpostfxPlay("HeistCelebPass", 10000001, true)
-    ShakeGameplayCam("DRUNK_SHAKE", 3.0)
+  RequestAnimSet("move_m@hobo@a") 
+  while not HasAnimSetLoaded("move_m@hobo@a") do
+    Citizen.Wait(0)
+  end    
+  DisplayNotification('You took some Heroin!')
+  TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_SMOKING_POT", 0, 1)
+  Citizen.Wait(3000)
+  ClearPedTasksImmediately(playerPed)
+  SetTimecycleModifier("spectator3")
+  SetPedMotionBlur(playerPed, true)
+  SetPedMovementClipset(playerPed, "move_m@hobo@a", true)
+  SetPedIsDrunk(playerPed, true)
+  AnimpostfxPlay("HeistCelebPass", 10000001, true)
+  ShakeGameplayCam("DRUNK_SHAKE", 3.0)
 --vvvvvvvvvvvvvvvv
-    Citizen.Wait(DURATION)
+  Citizen.Wait(DURATION)
 --^^^^^^^^^^^^^^^^
 --Time of effect
 --  after wait stop all effects
-    SetPedIsDrunk(GetPlayerPed(-1), false)		
-    SetPedMotionBlur(playerPed, false)
-    ResetPedMovementClipset(GetPlayerPed(-1))
-    AnimpostfxStopAll()
-    ShakeGameplayCam("DRUNK_SHAKE", 0.0)
-    SetTimecycleModifierStrength(0.0)
+  SetPedIsDrunk(GetPlayerPed(-1), false)		
+  SetPedMotionBlur(playerPed, false)
+  ResetPedMovementClipset(GetPlayerPed(-1))
+  AnimpostfxStopAll()
+  ShakeGameplayCam("DRUNK_SHAKE", 0.0)
+  SetTimecycleModifierStrength(0.0)
 end
 
 function lsd()
   local playerPed = GetPlayerPed(-1)
   local playerPed = PlayerPedId()
   
-    RequestAnimSet("move_m@buzzed") 
-    while not HasAnimSetLoaded("move_m@buzzed") do
-      Citizen.Wait(0)
-    end    
-    DisplayNotification('You took some LSD!')
-    TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_SMOKING_POT", 0, 1)
-    Citizen.Wait(3000)
-    ClearPedTasksImmediately(playerPed)
-    SetPedMotionBlur(playerPed, true)
-    SetPedMovementClipset(playerPed, "move_m@buzzed", true)
-    SetPedIsDrunk(playerPed, true)
-    SetTimecycleModifier("spectator5")
-    AnimpostfxPlay("Rampage", 10000001, true)
-    ShakeGameplayCam("DRUNK_SHAKE", 1.5)
+  RequestAnimSet("move_m@buzzed") 
+  while not HasAnimSetLoaded("move_m@buzzed") do
+    Citizen.Wait(0)
+  end    
+  DisplayNotification('You took some LSD!')
+  TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_SMOKING_POT", 0, 1)
+  Citizen.Wait(3000)
+  ClearPedTasksImmediately(playerPed)
+  SetPedMotionBlur(playerPed, true)
+  SetPedMovementClipset(playerPed, "move_m@buzzed", true)
+  SetPedIsDrunk(playerPed, true)
+  SetTimecycleModifier("spectator5")
+  AnimpostfxPlay("Rampage", 10000001, true)
+  ShakeGameplayCam("DRUNK_SHAKE", 1.5)
 --vvvvvvvvvvvvvvvv
-    Citizen.Wait(DURATION)
+  Citizen.Wait(DURATION)
 --^^^^^^^^^^^^^^^^
 --Time of effect
 --  after wait stop all effects
-    SetPedIsDrunk(GetPlayerPed(-1), false)		
-    SetPedMotionBlur(playerPed, false)
-    ResetPedMovementClipset(GetPlayerPed(-1))
-    AnimpostfxStopAll()
-    ShakeGameplayCam("DRUNK_SHAKE", 0.0)
-    SetTimecycleModifierStrength(0.0)
+  SetPedIsDrunk(GetPlayerPed(-1), false)		
+  SetPedMotionBlur(playerPed, false)
+  ResetPedMovementClipset(GetPlayerPed(-1))
+  AnimpostfxStopAll()
+  ShakeGameplayCam("DRUNK_SHAKE", 0.0)
+  SetTimecycleModifierStrength(0.0)
 end
 
 function meth()
   local playerPed = GetPlayerPed(-1)
   local playerPed = PlayerPedId()
   
-    RequestAnimSet("move_m@drunk@slightlydrunk") 
-    while not HasAnimSetLoaded("move_m@drunk@slightlydrunk") do
-      Citizen.Wait(0)
-    end    
-    DisplayNotification('You took some Meth!')
-    TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_SMOKING_POT", 0, 1)
-    Citizen.Wait(3000)
-    ClearPedTasksImmediately(playerPed)
-    SetPedMotionBlur(playerPed, true)
-    SetPedMovementClipset(playerPed, "move_m@drunk@slightlydrunk", true)
-    SetPedIsDrunk(playerPed, true)
-    SetTimecycleModifier("spectator5")
-    AnimpostfxPlay("SuccessMichael", 10000001, true)
-    ShakeGameplayCam("DRUNK_SHAKE", 1.5)
+  RequestAnimSet("move_m@drunk@slightlydrunk") 
+  while not HasAnimSetLoaded("move_m@drunk@slightlydrunk") do
+    Citizen.Wait(0)
+  end    
+  DisplayNotification('You took some Meth!')
+  TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_SMOKING_POT", 0, 1)
+  Citizen.Wait(3000)
+  ClearPedTasksImmediately(playerPed)
+  SetPedMotionBlur(playerPed, true)
+  SetPedMovementClipset(playerPed, "move_m@drunk@slightlydrunk", true)
+  SetPedIsDrunk(playerPed, true)
+  SetTimecycleModifier("spectator5")
+  AnimpostfxPlay("SuccessMichael", 10000001, true)
+  ShakeGameplayCam("DRUNK_SHAKE", 1.5)
 --vvvvvvvvvvvvvvvv
-    Citizen.Wait(DURATION)
+  Citizen.Wait(DURATION)
 --^^^^^^^^^^^^^^^^
 --Time of effect
 --  after wait stop all effects
-    SetPedIsDrunk(GetPlayerPed(-1), false)		
-    SetPedMotionBlur(playerPed, false)
-    ResetPedMovementClipset(GetPlayerPed(-1))
-    AnimpostfxStopAll()
-    ShakeGameplayCam("DRUNK_SHAKE", 0.0)
-    SetTimecycleModifierStrength(0.0)
+  SetPedIsDrunk(GetPlayerPed(-1), false)		
+  SetPedMotionBlur(playerPed, false)
+  ResetPedMovementClipset(GetPlayerPed(-1))
+  AnimpostfxStopAll()
+  ShakeGameplayCam("DRUNK_SHAKE", 0.0)
+  SetTimecycleModifierStrength(0.0)
 end
 
 function lsa()
   local playerPed = GetPlayerPed(-1)
   local playerPed = PlayerPedId()
   
-    RequestAnimSet("move_m@buzzed") 
-    while not HasAnimSetLoaded("move_m@buzzed") do
-      Citizen.Wait(0)
-    end    
-    DisplayNotification('You took some LSA!')
-    TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_SMOKING_POT", 0, 1)
-    Citizen.Wait(3000)
-    ClearPedTasksImmediately(playerPed)
-    SetPedMotionBlur(playerPed, true)
-    SetPedMovementClipset(playerPed, "move_m@buzzed", true)
-    SetPedIsDrunk(playerPed, true)
-    SetTimecycleModifier("spectator5")
-    AnimpostfxPlay("Rampage", 10000001, true)
-    ShakeGameplayCam("DRUNK_SHAKE", 1.5)
+  RequestAnimSet("move_m@buzzed") 
+  while not HasAnimSetLoaded("move_m@buzzed") do
+    Citizen.Wait(0)
+  end    
+  DisplayNotification('You took some LSA!')
+  TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_SMOKING_POT", 0, 1)
+  Citizen.Wait(3000)
+  ClearPedTasksImmediately(playerPed)
+  SetPedMotionBlur(playerPed, true)
+  SetPedMovementClipset(playerPed, "move_m@buzzed", true)
+  SetPedIsDrunk(playerPed, true)
+  SetTimecycleModifier("spectator5")
+  AnimpostfxPlay("Rampage", 10000001, true)
+  ShakeGameplayCam("DRUNK_SHAKE", 1.5)
 --vvvvvvvvvvvvvvvv
-    Citizen.Wait(DURATION)
+  Citizen.Wait(DURATION)
 --^^^^^^^^^^^^^^^^
 --Time of effect
 --  after wait stop all effects
-    SetPedIsDrunk(GetPlayerPed(-1), false)		
-    SetPedMotionBlur(playerPed, false)
-    ResetPedMovementClipset(GetPlayerPed(-1))
-    AnimpostfxStopAll()
-    ShakeGameplayCam("DRUNK_SHAKE", 0.0)
-    SetTimecycleModifierStrength(0.0)
+  SetPedIsDrunk(GetPlayerPed(-1), false)		
+  SetPedMotionBlur(playerPed, false)
+  ResetPedMovementClipset(GetPlayerPed(-1))
+  AnimpostfxStopAll()
+  ShakeGameplayCam("DRUNK_SHAKE", 0.0)
+  SetTimecycleModifierStrength(0.0)
 end
 
 function sober()
@@ -227,6 +255,9 @@ function sober()
 end
 
 -- // EVENTS
+RegisterNetEvent('kypo-drug-effect:onBeer')
+AddEventHandler('kypo-drug-effect:onBeer', beer)
+
 RegisterNetEvent('kypo-drug-effect:onCoke')
 AddEventHandler('kypo-drug-effect:onCoke', coke)
 
@@ -249,6 +280,10 @@ RegisterNetEvent('kypo-drug-effect:onSober')
 AddEventHandler('kypo-drug-effect:onSober', sober)
 
 -- // COMMANDS
+RegisterCommand("beer", function(source,args,rawCommand)
+  beer()
+end, false)
+
 RegisterCommand("coke", function(source,args,rawCommand)
   coke()
 end, false)
